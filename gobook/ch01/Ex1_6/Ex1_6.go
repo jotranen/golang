11382020be1905c051e1850b3567966d7f087d4f
Jari Otranen <jari.otranen@gmail.com>
2017-01-29 21:43:01 +0200

    ch01 ex1_5, ex1_6

---
+++ gobook/ch01/Ex1_6/Ex1_6.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"image/color"
+	"os"
+	"io"
+	"math/rand"
+	"image/gif"
+	"image"
+	"math"
+)
+
+var palette = []color.Color{color.Black,
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xff, 0xff}}
+
+const (
+	WhiteIndex = 0
+	redIndex = 1
+	greenIndex = 2
+	blueIndex = 3
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles = 5
+		res = 0.001
+		size = 100
+		nframes = 64
+		delay = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		var index uint8 = 0
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+
+			// exciting rotation...
+			index += 1
+			if index > 3 {
+				index = 0
+			}
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
