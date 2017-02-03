233a44ea89887593cf150c2047cbcd9c7ff6ed37
Jari Otranen <jari.otranen@gmail.com>
2017-02-03 15:02:08 +0200

    ch02 ex2_3, ex2_4, ex2_5

---
+++ gobook/ch02/Ex2_3/Ex2_3.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pc[256]byte
+
+func init() {
+	for i:= range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+	pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2(x uint64) uint {
+	// see performance comparison between loops and iterators
+	// http://stackoverflow.com/questions/21950244/is-there-a-way-to-iterate-over-a-range-of-integers-in-golang
+
+	var count uint = 0
+
+	for x >  0 {
+		if ((x & 1) == 1) {
+			count++
+		}
+		x >>= 1
+
+	}
+
+	return count
+}
+
+func main() {
+	start := time.Now()
+	fmt.Printf("PopCount: %d\n", PopCount(10))
+	elapsed1 := time.Since(start)
+
+	start = time.Now()
+	fmt.Printf("PopCount: %d\n", PopCount2(10))
+	elapsed2 := time.Since(start)
+
+	fmt.Printf("Single expression : %s\n", elapsed1)
+	fmt.Printf("Looping solution  : %s\n", elapsed2)
+}
+
---
+++ gobook/ch02/Ex2_5/Ex2_5.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var pc[256]byte
+
+func init() {
+	for i:= range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2_4(x uint64) uint {
+	// see performance comparison between loops and iterators
+	// http://stackoverflow.com/questions/21950244/is-there-a-way-to-iterate-over-a-range-of-integers-in-golang
+
+	var count uint = 0
+
+	for x >  0 {
+		if ((x & 1) == 1) {
+			count++
+		}
+		x >>= 1
+
+	}
+
+	return count
+}
+
+func PopCount2_5(x uint64) uint {
+	var count uint
+	for count = 0; x != 0; count++ {
+		x &= x -1
+	}
+	return count
+}
+
+func main() {
+	start := time.Now()
+	fmt.Printf("PopCount: %d\n", PopCount(10))
+	elapsed1 := time.Since(start)
+
+	start = time.Now()
+	fmt.Printf("PopCount: %d\n", PopCount2_4(10))
+	elapsed2 := time.Since(start)
+
+	start = time.Now()
+	fmt.Printf("PopCount: %d\n", PopCount2_5(10))
+	elapsed3 := time.Since(start)
+
+	fmt.Printf("Single expression   : %s\n", elapsed1)
+	fmt.Printf("Looping solution    : %s\n", elapsed2)
+	fmt.Printf("Rightmost algorithm : %s\n", elapsed3)
+}
+
