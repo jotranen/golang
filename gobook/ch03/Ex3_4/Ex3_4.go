3f988ddb12c122e6b17811c9898ab56c0bddb263
Jari Otranen <jari.otranen@gmail.com>
2017-02-03 22:49:34 +0200

    ch03 ex3_4

---
+++ gobook/ch03/Ex3_4/Ex3_4.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"math"
+	"fmt"
+	"net/http"
+	"log"
+	"io"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
+
+func main() {
+	http.HandleFunc("/surface", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	//u, err := url.Parse(r.URL.String())
+
+	w.Header().Set("Content-Type", "image/svg+xml")
+	surface(w)
+}
+
+func surface(out io.Writer) {
+	out.Write([]byte(fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' " +
+		"style='stroke: grey; fill: white; stroke-width: 0.7' " +
+		"width='%d' height='%d'>\n", width, height)))
+
+	for i := 0; i <  cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			if !(math.IsInf(ax, 0) ||
+				math.IsInf(ax, 0) ||
+				math.IsInf(ay, 0) ||
+				math.IsInf(bx, 0) ||
+				math.IsInf(by, 0) ||
+				math.IsInf(cx, 0) ||
+				math.IsInf(cy, 0) ||
+				math.IsInf(dx, 0) ||
+				math.IsInf(dy, 0)) {
+				out.Write([]byte(fmt.Sprintf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)))
+			}
+
+		}
+	}
+	out.Write([]byte(fmt.Sprintln("</svg>")))
+
+}
+
+func corner(i, j int) (float64, float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x,y)
+
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	return sx, sy
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y)
+
+	return math.Sin(r) / r
+}
