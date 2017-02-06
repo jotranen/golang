4ec8f352cf20ffdacc47880e6facd8f368bde7b8
Jari Otranen <jari.otranen@gmail.com>
2017-02-06 22:57:41 +0200

    ch04 work

---
+++ gobook/ch04/Ex4_1/Ex4_1.go
@@ -0,0 +1,32 @@
+package main
+
+// TODO: some testing if this actually is correct...
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+
+	fmt.Printf("Number of different bytes: %d\n", numOfDifferentBytes(&c1, &c2))
+}
+
+func numOfDifferentBytes(c1 *[32]byte, c2 *[32]byte) uint {
+	res := uint(0)
+	for i := range c1 {
+		tmp := c1[i] ^ c2[i]
+		res += PopCount2_5(tmp)
+	}
+	return res
+}
+
+func PopCount2_5(x uint8) uint {
+	var count uint
+	for count = 0; x != 0; count++ {
+		x &= x -1
+	}
+	return count
+}
---
+++ gobook/ch04/Ex4_2/Ex4_2.go
@@ -0,0 +1,5 @@
+package Ex4_2
+
+// TODO: todo
+
+
