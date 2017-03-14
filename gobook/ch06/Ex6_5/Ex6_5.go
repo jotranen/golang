aa4d9fbfb7f8b9ab3058dad3d0f4dff4c4553f6f
Jari Otranen <jari.otranen@gmail.com>
2017-03-15 00:19:43 +0200

    ex7_2

--- gobook/ch06/Ex6_5/Ex6_5.go
+++ gobook/ch06/Ex6_5/Ex6_5.go
@@ -1,3 +1,7 @@
 // TODO
 
 package Ex6_5
+
+/*
+ * Exercise 6.5: The type of each word used by IntSet is uint64, but 64-bit arithmetic may be inefficient on a 32-bit platform. Modify the program to use the uint type, which is the most efficient unsigned integer type for the platform. Instead of dividing by 64, define a constant holding the effective size of uint in bits, 32 or 64. You can use the perhaps too-clever expression 32 < < (^ uint( 0) > > 63) for this purpose.
+ */
---
+++ gobook/ch07/Ex7_2/Ex7_2.go
@@ -0,0 +1,41 @@
+// TODO
+
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+)
+
+/*
+ * Exercise 7.2: Write a function CountingWriter with the signature below that, given an io.Writer,
+ * returns a new Writer that wraps the original, and a pointer to an int64 variable that
+ * at any moment contains the number of bytes written to the new Writer.
+ *
+ * func CountingWriter( w io.Writer) (io.Writer, *int64)
+ */
+
+func main() {
+	writer, cnt := CountingWriter(os.Stdout)
+	fmt.Printf("Count: %d\n", *cnt)
+	fmt.Fprintf(writer, "test 123\n")
+	fmt.Printf("Count: %d\n", *cnt)
+
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	cnt := Counter{w, 0}
+	return &cnt, &cnt.cnt
+}
+
+type Counter struct {
+	w 	io.Writer
+	cnt	int64
+}
+
+func (c *Counter) Write(p []byte)(int, error) {
+	cnt, error := c.w.Write(p)
+	c.cnt += int64(cnt)
+	return cnt, error
+}
