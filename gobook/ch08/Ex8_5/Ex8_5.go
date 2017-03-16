d71be2349cac5f96c4823d9491eb9cb3eb4fb5fa
Jari Otranen <jari.otranen@gmail.com>
2017-03-16 23:52:00 +0200

    exercises

---
+++ gobook/ch08/Ex8_5/Ex8_5.go
@@ -0,0 +1,7 @@
+// TODO
+
+package Ex8_5
+
+/*
+ * Exercise 8.5: Take an existing CPU-bound sequential program, such as the Mandelbrot program of Section 3.3 or the 3-D surface computation of Section 3.2, and execute its main loop in parallel using channels for communication. How much faster does it run on a multiprocessor machine? What is the optimal number of goroutines to use?
+ */
---
+++ gobook/ch08/Ex8_6/Ex8_6.go
@@ -0,0 +1,7 @@
+// TODO
+
+package Ex8_6
+
+/*
+ * Exercise 8.6: Add depth-limiting to the concurrent crawler. That is, if the user sets -depth = 3, then only URLs reachable by at most three links will be fetched.
+ */
